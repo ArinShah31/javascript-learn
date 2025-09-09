f51f72329beb31406e8cda4e470b11a486964572
Arin Shah <arinshah31@gmail.com>
2025-09-09 14:23:54 +0530

    datatype summary

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -31,3 +31,36 @@ console.log(typeof nul); //object (this is a known quirk in JavaScript)
 console.log(typeof obj); //object
 console.log(typeof greet); //function
 console.log(typeof arr); //object (arrays are a type of object in JavaScript)   
+
+
+//memory types : stack and heap
+//primitive types are stored in stack memory
+//non-primitive types are stored in heap memory and a reference to that memory is stored in stack memory
+
+//example
+
+let myYoutubeChannel = "Codevolution"; //primitive type stored in stack memory
+let anotherChannel = myYoutubeChannel; //another reference in stack memory
+
+console.log(myYoutubeChannel); //Codevolution
+console.log(anotherChannel); //Codevolution
+
+anotherChannel = "Techsith"; //changing anotherChannel does not affect myYoutubeChannel
+
+console.log(myYoutubeChannel); //Codevolution
+console.log(anotherChannel); //Techsith
+
+let userOne ={
+    email: "user@gmail.com", //non-primitive type stored in heap memory
+    upi:"user@oksbi" //non-primitive type stored in heap memory
+    }
+
+let userTwo = userOne; //userTwo references the same object in heap memory
+
+console.log(userOne.email); //
+console.log(userTwo.email); //
+
+userTwo.email = "somethingelse@gmail.com"; //changing userTwo also affects userOne since both reference the same object
+
+console.log(userOne.email); //
+console.log(userTwo.email); //
