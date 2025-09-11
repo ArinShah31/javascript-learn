652eefb395699078da1a26e28a9042aded5f8a91
ArinShah31 <arinshah31@gmail.com>
2025-09-11 10:52:13 +0530

    learn scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,3 +6,48 @@ if (true) {
 } // local scope
 
 console.log(a);
+
+function one(){
+    const username = "arin"
+
+    function two(){
+        const website = "arin.com"
+        console.log(username); //can access parent scope variable so it wont throw error
+        console.log(website);
+    }
+    // console.log(website);//error
+    two()
+}
+one()
+
+// if else 
+
+    if (true) {
+        const username = "arin"
+        if(username === "arin"){
+            const website = " youtube"
+            console.log(username + website);
+        }
+        //console.log(website); // will throw error
+    }
+    // console.log(username); // will throw error as it is inside if block which is a local scope
+
+// ++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++
+// there are two ways to create a function in js
+// 1. function declaration
+// 2. function expression
+
+// function declaration
+addOne(5) //6 // can be called before declaration
+function addOne(num1){
+    return num1 + 1
+}
+
+
+// function expression
+
+//addTwo(5) //7 // will throw error if called before declaration
+const addTwo = function(num1){
+    return num1 + 2
+}
+addTwo(5) //7
