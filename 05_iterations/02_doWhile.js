bf28edf58360884b430e31363ce8b1d75ce11d35
ArinShah31 <arinshah31@gmail.com>
2025-09-11 16:51:11 +0530

    doWhile

---
+++ 05_iterations/02_doWhile.js
@@ -0,0 +1,22 @@
+// while looops
+let i = 0;
+while (i<=10) {
+    console.log(`value of i is ${i}`);
+    i = i+2
+}
+
+let arr= ["batman", "superman", "wonderwoman", "aquaman"];
+
+let z=0;
+while(z < arr.length){
+    console.log(`the superhero is ${arr[z]}`);
+    z++
+}
+    
+    //dowhile loop
+
+    let score = 0;
+    do {
+        console.log(`your score is ${score}`);
+        score++;
+    } while (score < 5);
